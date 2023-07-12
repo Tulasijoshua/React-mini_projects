d21bdfff2f7b0cbd95e9b7b7d72a96b0349699d4
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-07-12 03:39:52 +0000

    product images file

---
+++ basic-ecommerce-app/src/assets/ProductImg.jsx
@@ -0,0 +1,55 @@
+import ld1 from './Images/ladiesWear/dresses/d1.jpg'
+import ld2 from './Images/ladiesWear/dresses/d2.jpg'
+import ld3 from './Images/ladiesWear/dresses/d3.jpg'
+import ld4 from './Images/ladiesWear/dresses/d4.jpg'
+import ld5 from './Images/ladiesWear/dresses/d5.jpg'
+import lj1 from './Images/ladiesWear/jeans/j1.jpg'
+import lj2 from './Images/ladiesWear/jeans/j2.jpg'
+import lj3 from './Images/ladiesWear/jeans/j3.jpg'
+import lj4 from './Images/ladiesWear/jeans/j4.jpg'
+import lj5 from './Images/ladiesWear/jeans/j5.jpg'
+import lt1 from './Images/ladiesWear/trousers/t1.jpg'
+import lt2 from './Images/ladiesWear/trousers/t2.jpg'
+import lt3 from './Images/ladiesWear/trousers/t3.jpg'
+import lt4 from './Images/ladiesWear/trousers/t4.jpg'
+import lt5 from './Images/ladiesWear/trousers/t5.jpg'
+
+
+// Men products
+
+import mh1 from './Images/men/hoodie/h1.jpg'
+import mh2 from './Images/men/hoodie/h2.jpg'
+import mh3 from './Images/men/hoodie/h3.jpg'
+import mh4 from './Images/men/hoodie/h4.jpg'
+import mh5 from './Images/men/hoodie/h5.jpg'
+import mj1 from './Images/men/jeans/mj-1.jpg'
+import mj2 from './Images/men/jeans/mj-2.jpg'
+import mj3 from './Images/men/jeans/mj-3.jpg'
+import mj4 from './Images/men/jeans/mj-4.jpg'
+import mj5 from './Images/men/jeans/mj-5.jpg'
+import ms1 from './Images/men/shirt/ms-1.jpg'
+import ms2 from './Images/men/shirt/ms-2.jpg'
+import ms3 from './Images/men/shirt/ms-3.jpg'
+import ms4 from './Images/men/shirt/ms-4.jpg'
+import ms5 from './Images/men/shirt/ms-5.jpg'
+
+// Children products
+import cs1 from './Images/children/shirt/cs-1.jpg'
+import cs2 from './Images/children/shirt/cs-2.jpg'
+import cs3 from './Images/children/shirt/cs-3.jpg'
+import cs4 from './Images/children/shirt/cs-4.jpg'
+import cs5 from './Images/children/shirt/cs-5.jpg'
+import ch1 from './Images/children/hoodie/ch-1.jpg'
+import ch2 from './Images/children/hoodie/ch-2.jpg'
+import ch3 from './Images/children/hoodie/ch-3.jpg'
+import ch4 from './Images/children/hoodie/ch-4.jpg'
+import ch5 from './Images/children/hoodie/ch-5.jpg'
+import ks1 from './Images/children/suit/ks-1.jpg'
+import ks2 from './Images/children/suit/ks-2.jpg'
+import ks3 from './Images/children/suit/ks-3.jpg'
+import ks4 from './Images/children/suit/ks-4.jpg'
+import ks5 from './Images/children/suit/ks-5.jpg'
+
+export {ld1, ld2, ld3, ld4, ld5, lj1, lj2, lj3, lj4, lj5, lt1, lt2, lt3, lt4, lt5,
+        mh1, mh2, mh3, mh4, mh5, mj1, mj2, mj3, mj4, mj5, ms1, ms2, ms3, ms4, ms5,
+        cs1, cs2, cs3, cs4, cs5, ch1, ch2, ch3, ch4, ch5, ks1, ks2, ks3, ks4, ks5}
